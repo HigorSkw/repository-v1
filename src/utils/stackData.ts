d1e46d4c037b21e1f19eae9e627c0b196f26da79
HigorSkw <higor.skowronski@gmail.com>
2023-01-30 02:15:07 -0300

    v1-portfolio

---
+++ src/utils/stackData.ts
@@ -0,0 +1,40 @@
+import { SiTypescript, SiStyledcomponents } from "react-icons/si";
+import {
+  FaDocker,
+  FaHtml5,
+  FaCss3Alt,
+  FaJs,
+  FaNode,
+  FaReact,
+  FaPython,
+} from "react-icons/fa";
+
+export const stackData = [
+  {
+    title: "React",
+    img: FaReact,
+  },
+  {
+    title: "HTML",
+    img: FaHtml5,
+  },
+  {
+    title: "CSS",
+    img: FaCss3Alt,
+  },
+  {
+    title: "JS",
+    img: FaJs,
+  },
+  {
+    title: "Node JS",
+    img: FaNode,
+  },
+  {
+    title: "Styled Components",
+    img: SiStyledcomponents,
+  },
+  { title: "TypeScript", img: SiTypescript },
+  { title: "Python", img: FaPython },
+  { title: "Docker", img: FaDocker },
+];
---
+++ src/utils/userData.ts
@@ -0,0 +1,6 @@
+export const userData = {
+  nameUser: "Higor Skowronski",
+  githubUser: "higorskw",
+  linkedinUser: "higorskw",
+  whatsappNumber: "+5548998522681",
+};
