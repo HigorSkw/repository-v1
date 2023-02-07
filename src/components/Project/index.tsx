218c3f075c480859433f7044cc7e66fb69a161c1
HigorSkw <higor.skowronski@gmail.com>
2023-02-07 00:07:44 -0300

    fix: url projects

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -17,6 +17,7 @@ interface ReposType {
   language: string;
   description: string;
   git_url: string;
+  html_url: string;
   homepage: string;
 }
 
@@ -27,7 +28,7 @@ export const Project = (): JSX.Element => {
     const fetchData = async () => {
       const data: Response = await fetch(
         `https://api.github.com/users/${userData.githubUser}/repos`
-      )
+      );
 
       const json = await data.json();
 
@@ -41,6 +42,8 @@ export const Project = (): JSX.Element => {
     };
     fetchData();
   }, []);
+
+  console.log(repositories);
 
   return (
     <>
@@ -70,7 +73,7 @@ export const Project = (): JSX.Element => {
             {repository.description}
           </Text>
           <ProjectLinks>
-            <ProjectLink target="_blank" href={repository.git_url}>
+            <ProjectLink target="_blank" href={repository.html_url}>
               <FaGithub /> Github Code
             </ProjectLink>
             {repository.homepage && (
